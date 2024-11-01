0ee00634e42b18b92614e62f5549f2787100f920
kevynlope <lopes.kevyn@escola.pr.gov.br>
2024-11-01 18:22:19 +0000

    setTimeout

--- funcoes/setTimeOut.js
+++ funcoes/setTimeOut.js
@@ -3,3 +3,17 @@ setTimeout(function(){
     console.log("tempo esgotado")
 }, 4000)
 
+//setInterval (função, tempo ms)
+let fps = Math.round(Math.random() * 30)
+const intervalo1 = setInterval (function() {
+    console.clear()
+    console.log("fps:" + fps)
+    fps = fps + 1
+}, 1000)
+
+setTimeout(function(){
+    clearInterval(intervalo1)
+    console.log("Conexão")
+}, 5000)
+
+
